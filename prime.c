21e5214e20b0b204e6e1c87b31355039a7776174
Proma <promaafia@gmail.com>
2019-10-18 13:57:53 +0600

    add loop

--- prime.c
+++ prime.c
@@ -3,5 +3,20 @@
     int main()
     {
        
+for ( count = 2 ; count = n ;  )
+       {
+          for ( c = 2 ; c = i - 1 ; c++ )
+          {
+             if ( i%c == 0 )
+                break;
+          }
+          if ( c == i )
+          {
+             printf(%dn, i);
+             count++;
+          }
+          i++;
+       }
+     
        return 0;
     }
